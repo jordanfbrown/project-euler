96396a50de94fb917b50e815dc8dae6886593f4b
Jordan Brown <jordanfbrown@gmail.com>
2013-04-07 23:04:36 -0700

    Problem 9

--- problem9.rb
+++ problem9.rb
@@ -1,4 +1,10 @@
-a = b = c = 0
-
-puts a, b, c
-
+(1..1000).each do |a|
+  (1..1000).each do |b|
+    (1..1000).each do |c|
+      if a + b + c == 1000 && a ** 2 + b ** 2 - c ** 2 == 0
+        puts a * b * c
+        break
+      end
+    end
+  end
+end
