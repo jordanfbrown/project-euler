4777a00893d1c1ae6402645d7a5e448e2873da9f
Jordan Brown <jordanfbrown@gmail.com>
2013-04-07 22:54:15 -0700

    Problem 8

--- problem24.rb
+++ problem24.rb
@@ -1,20 +1,14 @@
 def next_permutation(array)
   max_k = 0
   for k in 0..array.length - 1
-    if !array[k+1].nil? && array[k] < array[k + 1] && k > max_k
-      max_k = k
-    end
+    max_k = k if !array[k+1].nil? && array[k] < array[k + 1] && k > max_k
   end
 
-  if max_k == -1
-    return array
-  end
+  return array if max_k == -1
 
   max_l = 0
   for l in 0..array.length - 1
-    if array[max_k] < array[l] && l > max_l
-      max_l = l
-    end
+    max_l = l if array[max_k] < array[l] && l > max_l
   end
 
   array[max_k], array[max_l] = array[max_l], array[max_k]
